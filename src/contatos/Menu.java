7c00c4a0db8fe479c8719a0eef1ed9db1e29dccb
jloliveira95 <jl.oliveira95@gmail.com>
2024-12-19 11:53:44 -0300

    Adicionando código da classe Menu

--- src/contatos/Menu.java
+++ src/contatos/Menu.java
@@ -1,20 +1,71 @@
 package contatos;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+
 public class Menu {
 	
-	public static void main(String[] args) {
+	public static Scanner leia = new Scanner(System.in);
 	
-		System.out.println("****************************");
-		System.out.println("        MEUS CONTATOS       ");
-		System.out.println("****************************");
-		System.out.println("     1 - Criar Contato");
-		System.out.println("     2 - Listar Contatos");
-		System.out.println("     3 - Deletar Contato");
-		System.out.println("     4 - Atualizar Contato");
-		System.out.println("     5 - Sair");
-		System.out.println("****************************");
-		System.out.print("Digite a opção desejada: ");
-		System.out.println("\n****************************");
+	public static void main(String[] args) {
+		
+        int opcao = 0;
+		while (true) {
+            try {
+                System.out.println("****************************");
+                System.out.println("        MEUS CONTATOS       ");
+                System.out.println("****************************");
+                System.out.println("     1 - Criar Contato");
+                System.out.println("     2 - Listar Contatos");
+                System.out.println("     3 - Deletar Contato");
+                System.out.println("     4 - Atualizar Contato");
+                System.out.println("     5 - Sair");
+                System.out.println("****************************");
+                System.out.print("Digite a opção desejada: ");
+                opcao = leia.nextInt();
+			    System.out.println("\n****************************");
+                
+                if(opcao == 5) {
+                    System.out.println("Armazene seus contatosDe forma inteligente! Saindo...");
+                    leia.close();
+                    System.exit(0);
+                    break;
+                }
 
+                switch(opcao) {
+                    case 1:
+                        System.out.println("\nCriar Contato\n");
+                        
+                        break;
+                    
+                    case 2:
+                        System.out.println("\nListar Contatos\n");
+                        
+                        break;
+                        
+                    case 3:
+                        System.out.println("\nDeletar Contato\n");
+                        
+                        break;
+                        
+                    case 4:
+                        System.out.println("\nAtualizar Contato\n");
+                        
+                        break;
+                        
+                    default:
+                        System.out.println("\nOpção inválida!\n");
+
+                        break;
+                        
+                }
+            } catch (InputMismatchException e) {
+				System.out.println("Digite Valores Inteiros");
+				leia.nextLine(); // Limpa o buffer
+            }
+
+		}
 	}
 }
