27e4f07f682468e7d54fb3a2b9893df5c7c64ceb
jloliveira95 <jl.oliveira95@gmail.com>
2024-12-19 11:44:27 -0300

    Primeiro ciommit - Menu

---
+++ src/contatos/Menu.java
@@ -0,0 +1,20 @@
+package contatos;
+
+public class Menu {
+	
+	public static void main(String[] args) {
+	
+		System.out.println("****************************");
+		System.out.println("        MEUS CONTATOS       ");
+		System.out.println("****************************");
+		System.out.println("     1 - Criar Contato");
+		System.out.println("     2 - Listar Contatos");
+		System.out.println("     3 - Deletar Contato");
+		System.out.println("     4 - Atualizar Contato");
+		System.out.println("     5 - Sair");
+		System.out.println("****************************");
+		System.out.print("Digite a opção desejada: ");
+		System.out.println("\n****************************");
+
+	}
+}
